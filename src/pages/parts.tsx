20531a00d0e604c90b4a3010a2cc7ad7482706ec
davidruncode <daguz.91@gmail.com>
2025-07-08 21:28:31 -0400

    Parts

--- src/pages/parts.tsx
+++ src/pages/parts.tsx
@@ -1,11 +1,21 @@
+import { ChartAreaInteractive } from "@/components/chart-area-interactive";
+import { DataTable } from "@/components/data-table";
+import { SectionCards } from "@/components/section-cards";
+import { SiteHeader } from "@/components/site-header";
+import { SidebarInset } from "@/components/ui/sidebar";
+import data from "@/data/data.json";
+
 export default function Parts() {
   return (
-  <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-    <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-      <div className="bg-muted/50 aspect-video rounded-xl" />
-      <div className="bg-muted/50 aspect-video rounded-xl" />
-      <div className="bg-muted/50 aspect-video rounded-xl" />
-    </div>
-    <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
-  </div>
-)}
+
+      <SidebarInset>
+        <div className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+              <DataTable data={data} />
+            </div>
+          </div>
+        </div>
+      </SidebarInset>
+  );
+}
